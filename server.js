11f9cfb19f567304b40d9e4b2f9d60be11956515
pizzou <pmpumuropizzou@gmail.com>
2024-10-07 19:06:12 +0200

    first commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://lms-front-end-two.vercel.app/',
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
