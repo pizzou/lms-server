179101641a1bda23d37228492a340a9d02315716
pizzou <pmpumuropizzou@gmail.com>
2024-10-20 14:58:23 +0200

    commit 001

--- controllers/student-controller/course-controller.js
+++ controllers/student-controller/course-controller.js
@@ -88,27 +88,27 @@ const getStudentViewCourseDetails = async (req, res) => {
   }
 };
 
-const checkCoursePurchaseInfo = async (req, res) => {
-  try {
-    const { id, studentId } = req.params;
-    const studentCourses = await StudentCourses.findOne({
-      userId: studentId,
-    });
+// const checkCoursePurchaseInfo = async (req, res) => {
+//   try {
+//     const { id, studentId } = req.params;
+//     const studentCourses = await StudentCourses.findOne({
+//       userId: studentId,
+//     });
 
-    const ifStudentAlreadyBoughtCurrentCourse =
-      studentCourses.courses.findIndex((item) => item.courseId === id) > -1;
-    res.status(200).json({
-      success: true,
-      data: ifStudentAlreadyBoughtCurrentCourse,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
-    });
-  }
-};
+//     const ifStudentAlreadyBoughtCurrentCourse =
+//       studentCourses.courses.findIndex((item) => item.courseId === id) > -1;
+//     res.status(200).json({
+//       success: true,
+//       data: ifStudentAlreadyBoughtCurrentCourse,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
 
 module.exports = {
   getAllStudentViewCourses,
